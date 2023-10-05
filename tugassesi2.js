aadaff8c4d289325527507f5a7f6488a531f6d68
Ginanurbilkis <114636953+Ginanurbilkis@users.noreply.github.com>
2023-10-05 14:47:55 +0700

    Add files via upload

---
+++ tugassesi2.js
@@ -0,0 +1,11 @@
+const myBiodata = {
+    firstName : "Gina",
+    lastName  : "Nurbilkis",
+    age       : 18,
+    isMarried : "Lajang",
+
+}
+
+console.log(`Nama ${myBiodata.firstName} ${myBiodata.lastName}`);
+console.log(`Umur ${myBiodata.age}`);
+console.log(`dan saya masih ${myBiodata.isMarried}`);
